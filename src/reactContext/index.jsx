7ffc1280a6683293c1b73e71fe70f730a2382223
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2023-07-31 10:58:19 +0100

    updated the react context concept

--- src/reactContext/index.jsx
+++ src/reactContext/index.jsx
@@ -3,21 +3,45 @@ import Section from "./component/Section";
 
 const ContextApp = () => {
   return (
-    <Section level={1}>
+    // <Section level={1}>
+    //   <Heading>Title</Heading>
+    //   <Section level={2}>
+    //     <Heading>Heading</Heading>
+    //     <Heading>Heading</Heading>
+    //     <Section level={3}>
+    //       <Heading>Heading</Heading>
+    //       <Heading>Heading</Heading>
+    //       <Section level={4}>
+    //         <Heading>Heading</Heading>
+    //         <Heading>Heading</Heading>
+    //         <Section level={5}>
+    //           <Heading>Heading</Heading>
+    //           <Heading>Heading</Heading>
+    //           <Section level={1}>
+    //             <Heading>Heading</Heading>
+    //             <Heading>Heading</Heading>
+    //             <Heading>Heading</Heading>
+    //           </Section>
+    //         </Section>
+    //       </Section>
+    //     </Section>
+    //   </Section>
+    // </Section>
+    <Section>
       <Heading>Title</Heading>
-      <Section level={2}>
+      <Section>
         <Heading>Heading</Heading>
         <Heading>Heading</Heading>
-        <Section level={3}>
+        <Section>
           <Heading>Heading</Heading>
           <Heading>Heading</Heading>
-          <Section level={4}>
+          <Section>
             <Heading>Heading</Heading>
             <Heading>Heading</Heading>
-            <Section level={5}>
+            <Section>
               <Heading>Heading</Heading>
               <Heading>Heading</Heading>
-              <Section level={1}>
+              <Section>
                 <Heading>Heading</Heading>
                 <Heading>Heading</Heading>
                 <Heading>Heading</Heading>
